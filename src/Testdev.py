2ef8eb4ec2b662766bc1b8334480232d427a91b9
rthan1 <ethanwnelson@gmail.com>
2025-11-08 01:14:18 -0500

    Save Us

--- src/Testdev.py
+++ src/Testdev.py
@@ -1,10 +1,20 @@
 from RpiMotorLib import RpiMotorLib
 from time import sleep
 
-motorPins = [17,18,27,22]
+# GPIO pins connected to ULN2003 driver IN1-IN4
+motorPins = [17, 18, 27, 22]
 
-# simple half-step sequence
-myMotor = RpiMotorLib.BYJMotor("28BYJ48",motor_pins = None)
+# Initialize motor
+myMotor = RpiMotorLib.BYJMotor("MyMotor", motor_type="28BYJ")
 
-myMotor.motor_run(motorPins, 0.002, 4096, False, False, "half", 0.05)
-
+# Rotate 1 full revolution (~4096 half-steps)
+# motor_run(pins, stepdelay, steps, clockwise, verbose, steptype, initdelay)
+myMotor.motor_run(
+    motorPins,    # GPIO pins
+    0.002,        # step delay
+    4096,         # number of steps (1 rev for 28BYJ-48)
+    False,        # clockwise or counterclockwise
+    False,         # verbose output
+    "half",       # step type: 'half' or 'full'
+    0.05          # initial delay
+)
